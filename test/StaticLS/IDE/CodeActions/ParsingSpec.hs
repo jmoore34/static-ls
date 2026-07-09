d129fba586d89ae7780d46099d41ae4134e7433a
Jonathan Moore <m361234@heb.com>
2026-07-09 10:11:16 -0500

    Revert ""does not have field" support"
    This reverts commit ad22aba516f40247c388d9c15bb3e8d0f91b701f.

--- test/StaticLS/IDE/CodeActions/ParsingSpec.hs
+++ test/StaticLS/IDE/CodeActions/ParsingSpec.hs
@@ -23,14 +23,6 @@ spec = do
                 In an equation for ‘person’: person = Person {}
             |]
       result `shouldBe` Just (normalize "Person", Nothing, fmap normalize ["firstName", "middleName", "lastName", "parents"])
-    it "parses does not have field" do
-      let result =
-            missingFields . normalize $
-              [trimming|
-                Constructor ‘Person’ does not have field
-                ‘middleName’
-            |]
-      result `shouldBe` Just (normalize "Person", Nothing, fmap normalize ["middleName"])
     it "parses missing strict fields" do
       let result =
             missingFields . normalize $
